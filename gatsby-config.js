c4d7a1c35025a6f881f7a105ab2c723b5da5a017
Valentin <shakhov.valentin@gmail.com>
2023-07-07 01:51:58 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,15 +3,15 @@
  */
 module.exports = {
     siteMetadata: {
-        title: `StunArt`,
-        siteUrl: `https://www.yourdomain.tld`
+        title: `Svetlana Tungusova Art`,
+        siteUrl: `https://www.stunart.me`
     },
     plugins: [{
             resolve: `gatsby-plugin-google-gtag`,
             options: {
                 // You can add multiple tracking ids and a pageview event will be fired for all of them.
                 trackingIds: [
-                    "GA-TRACKING_ID", // Google Analytics / GA
+                    "G-QELKN47GG3", // Google Analytics / GA
                     "AW-CONVERSION_ID", // Google Ads / Adwords / AW
                     "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
                 ],
