d482152a7c548242d0fb4c750350e9b95c780d5c
vv <shakhov.valentin@gmail.com>
2023-07-16 01:43:40 +0200

    aa

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,35 +7,12 @@ module.exports = {
         siteUrl: `https://www.stunart.me`
     },
     plugins: [{
-        resolve: `gatsby-plugin-google-gtag`,
+        resolve: `gatsby-plugin-gtag`,
         options: {
-            // You can add multiple tracking ids and a pageview event will be fired for all of them.
-            trackingIds: [
-                "G-QELKN47GG3", // Google Analytics / GA
-                //    "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-                //    "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-            ],
-            // This object gets passed directly to the gtag config command
-            // This config will be shared across all trackingIds
-            gtagConfig: {
-                optimize_id: "OPT_CONTAINER_ID",
-                anonymize_ip: true,
-                cookie_expires: 0,
-            },
-            // This object is used for configuration specific to this plugin
-            pluginConfig: {
-                // Puts tracking script in the head instead of the body
-                head: false,
-                // Setting this parameter is also optional
-                respectDNT: true,
-                // Avoids sending pageview hits from custom paths
-                exclude: ["/preview/**", "/do-not-track/me/too/"],
-                // Defaults to https://www.googletagmanager.com
-                origin: "https://www.stunart.me",
-                // Delays processing pageview events on route update (in milliseconds)
-                delayOnRouteUpdate: 0,
-            },
-        },
+            trackingId: "G-QELKN47GG3",
+            head: true,
+            anonymize: true
+        }
     }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
         resolve: 'gatsby-plugin-manifest',
         options: {
