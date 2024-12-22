983809c67cd321297e4713ab8ace8be75a48e31b
shakh <kortzapp@gmail.com>
2024-12-22 17:47:59 +0100

    new

--- src/pages/adr.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-const AboutPage = () => (
-    <main>
-        <h1>Request modification, deletion or any other actions regarding your account or personal data</h1>
-        <p><a href="mailto:contact@kortz.app?subject=AR">Send an email</a></p>
-    </main>
-)
-export default AboutPage
